91e2d00c62efe3df6471fb893520d97e1b93533b
NichLim <nicholaspointcom@gmail.com>
2018-12-03 20:17:33 -0300

    Questão 2 da Revisão para AV3 de POO

---
+++ src/entidades/Calculadora.java
@@ -0,0 +1,36 @@
+package entidades;
+
+import java.util.List;
+
+public class Calculadora {
+	
+	public double somar(double a, double b){
+		return a+b;
+	}
+	public double dividir(double a, double  b){
+		return a/b;
+	}
+	public double multiplicar(double a, double b){
+		return a*b;
+	}
+	public double subtrair(double a, double b){
+		return a-b;
+	}
+	
+	public int somar(int a, int b){
+		return a+b;
+	}
+	public int subtrair(int a, int b){
+		return a-b;
+	}
+	
+	public double mediaArray(List lista){
+		
+		for (int obj : lista) {
+			
+		}
+		
+		return media;
+		
+	}
+}
---
+++ src/principal/Principal.java
@@ -0,0 +1,19 @@
+package principal;
+
+import entidades.Calculadora;
+
+public class Principal {
+
+	public static void main(String[] args){
+		
+		Calculadora c = new Calculadora();
+		
+		System.out.println("Soma 3,2 + 8,8 = " +  c.somar(3.2, 8.8));
+		System.out.println("Dividir 42 / 4,2 = " + c.dividir(42, 4.2));
+		System.out.println("Multiplicar 3,14 x 3,14 = " + c.multiplicar(3.14, 3.14));
+		System.out.println("Subtrair 800 - 30,7 = " +  c.subtrair(800, 30.7));
+		System.out.println("Somar 30 + 39 = " + c.somar(30, 39) + " lol");
+		System.out.println("Subtrair 89 - 20 = " + c.subtrair(89, 20) + " lol");
+		
+	}
+}
